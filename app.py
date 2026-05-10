7490b6c12343d5dedb3bbc8c49ee4851c94bee70
huesoo4 <josemanuellopezhueso@gmail.com>
2026-05-10 03:06:54 +0200

    Función para cargar los datos del arhivo .json en el programa

--- app.py
+++ app.py
@@ -3,5 +3,9 @@ import json
 
 app = Flask(__name__)
 
+def load_planets():
+    with open("data/planets_info.json", "r", encoding="utf-8") as file:
+        return json.load(file)
+
 if __name__ == "__main__":
     app.run(debug=True)
