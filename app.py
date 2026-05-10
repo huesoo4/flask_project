4ea098800e2e7b13d4d64de5623a1d67df941918
huesoo4 <josemanuellopezhueso@gmail.com>
2026-05-10 04:33:31 +0200

    Rutas principales de la aplicación Flask

--- app.py
+++ app.py
@@ -6,6 +6,22 @@ app = Flask(__name__)
 def load_planets():
     with open("data/planets_info.json", "r", encoding="utf-8") as file:
         return json.load(file)
+    
+@app.route("/")
+def index():
+    planets = load_planets()
+
+
+@app.route("/planets")
+def planets():
+    all_planets = load_planets()
+
+
+@app.route("/planet/<position>")
+def planet_detail(position):
+    planets = load_planets()
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
